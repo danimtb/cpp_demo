4ebfcb38efcfe1b794c6ac3d716cdbebc8cc5cfc
danimtb <danimanzaneque@gmail.com>
2026-03-10 18:54:33 +0100

    update download_script

--- download_model.py
+++ download_model.py
@@ -1,15 +1,13 @@
 import torch
 
-# Cargamos el modelo
-model = torch.hub.load("intel-isl/MiDaS", "MiDaS_small")
+model_name = "DPT_Hybrid"
+
+model = torch.hub.load("intel-isl/MiDaS", model_name)
+
+model_type = "DPT_Hybrid"
+
+device = torch.device("cpu")
+model.to(device)
 model.eval()
 
-# Creamos la entrada de ejemplo
-example_input = torch.rand(1, 3, 384, 384)
-
-# Forzamos el trazado ignorando los chequeos de consistencia
-with torch.no_grad():
-    traced_script_module = torch.jit.trace(model, example_input, check_trace=False)
-
-traced_script_module.save("MiDaS_small.pt")
-print("¡Solucionado! 'MiDaS_small.pt' generado correctamente.")
+torch.save(model.state_dict(), f"models/{model_name}.pt")
