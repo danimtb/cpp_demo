59c39df6b9093b43317ba697ce29189218626c98
danimtb <danimanzaneque@gmail.com>
2026-03-11 11:32:53 +0100

    update download script

--- download_model.py
+++ download_model.py
@@ -1,13 +1,20 @@
 import torch
 
-model_name = "DPT_Hybrid"
+# MiDas_small, 256
+# MiDas, 384
+# DPT_Hybrid, 384
+# DPT_Large, 384
+# DPT_BEiT_L_512, 512
 
-model = torch.hub.load("intel-isl/MiDaS", model_name)
-
-model_type = "DPT_Hybrid"
-
-device = torch.device("cpu")
-model.to(device)
+model_type = "DPT_Large"
+model = torch.hub.load("intel-isl/MiDaS", model_type)
 model.eval()
 
-torch.save(model.state_dict(), f"models/{model_name}.pt")
+example_input = torch.rand(1, 3, 384, 384)
+    
+with torch.no_grad():
+    # check_trace=False ignores the 'len()' warnings in the ViT backbone
+    traced_model = torch.jit.trace(model, example_input, check_trace=False)
+
+output_path = f"models/{model_type}.pt"
+traced_model.save(output_path)
