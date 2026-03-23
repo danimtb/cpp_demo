79b7081b0a9b10824879ea6067b3fad4ee73ac18
Abril Rincón Blanco <abrilrb@jfrog.com>
2026-03-23 12:10:24 +0100

    As is

--- conanfile.py
+++ conanfile.py
@@ -1,12 +1,35 @@
 from conan import ConanFile
+from conan.tools.cmake import cmake_layout, CMake
 
 class Recipe(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "CMakeToolchain", "CMakeConfigDeps"
 
+    def layout(self):
+        cmake_layout(self)
+
     def requirements(self):
         self.requires("libtorch/2.9.1")
         self.requires("opencv/4.12.0")
-        self.requires("eigen/3.4.1", override=True)
-        self.requires("protobuf/6.32.1", override=True)
-        self.requires("cpuinfo/cci.20251210", override=True)
+        self.requires("cuda-toolkit/12.6.0")
+        
+        # PicRoutette-specific dependencies
+        self.requires("opengl/system")
+        self.requires("sdl/2.28.3")
+        self.requires("sdl_image/2.8.2")
+        self.requires("sdl_ttf/2.24.0")
+        self.requires("nlohmann_json/3.11.3")
+
+        self.requires("libtiff/4.7.1", override=True)
+
+
+    def build_requirements(self):
+        self.tool_requires("cuda-toolkit/<host_version>")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        if not self.conf.get("tools.build:skip_test", True):
+            cmake.ctest()
+
